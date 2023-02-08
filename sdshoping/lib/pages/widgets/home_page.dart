42ef9e3132876ed86e23fcbe16466f445bc210d1
sadat <sadatmahmud1@outlook.com>
2023-02-09 02:06:35 +0600

    added json file

--- sdshoping/lib/pages/widgets/home_page.dart
+++ sdshoping/lib/pages/widgets/home_page.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:sdshoping/pages/models/catalogue.dart';
 import 'package:sdshoping/pages/widgets/drawer.dart';
 import 'package:sdshoping/pages/widgets/item_widget.dart';
 import 'package:sdshoping/pages/widgets/item_widget.dart';
-class HomePage extends StatelessWidget {
+import 'package:flutter/src/services/asset_bundle.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Sadat's";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadingData();
+  }
+loadingData () async{
+   var catalougejson = await rootBundle.loadString("assets/files/catalogue.json");
+  var decodeData = jsonDecode(catalougejson);
+  var productsData = decodeData["products"];
+}
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
