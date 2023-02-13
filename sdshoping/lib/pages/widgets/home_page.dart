69c8c69653a1fcc3ae599e4b4d17addc6f3b1ea4
sadat <sadatmahmud1@outlook.com>
2023-02-13 20:59:26 +0600

    grid tile complet with box decoration

--- sdshoping/lib/pages/widgets/home_page.dart
+++ sdshoping/lib/pages/widgets/home_page.dart
@@ -58,13 +58,59 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(16.0),
         child:
             (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-                ? ListView.builder(
-                    // itemCount: CatalogueModel.items.length,
-                    itemCount: CatalogModel.items?.length,
-                    itemBuilder: (context, index) => ItemWidget(
-                      item: CatalogModel.items![index],
+                ? GridView.builder(gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+                  
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+
+                
+                ),
+                 itemBuilder: (context,index){
+                  final item = CatalogModel.items[index];
+                  return Card(
+                   
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      
+                      header: Container( 
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                          
+                          ),
+
+                          padding: EdgeInsets.all(8),
+
+                          decoration: BoxDecoration(
+                            
+                            color: Colors.deepOrange),
+
+                          ),
+                      child: Image.network(item.image),
+                      
+                      footer: Container( 
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(color: Colors.white),
+                          
+                          ),
+
+                          padding: EdgeInsets.all(8),
+
+                          decoration: BoxDecoration(
+                            
+                            color: Colors.deepOrange),
+
+                          ),
+                      
+                    
                     ),
-                  )
+                  );
+                 },
+                 itemCount: CatalogModel.items.length,
+                 )
                 : Center(
                     child: CircularProgressIndicator(),
                   ),
